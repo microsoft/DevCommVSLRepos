80f24ff359848a4ee7f4efd126945a7a78895989
Mia Wu <miywu@microsoft.com>
2021-02-03 09:39:17 +0800

    add click once wpf test

---
+++ WPF Samples/ClickOnceWpfTest/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += Grid_Loaded;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Properties.Settings.Default.Upgrade();
+            Properties.Settings.Default.TestSetting = "2";
+           
+            Properties.Settings.Default.Save();
+            MessageBox.Show(Properties.Settings.Default.TestSetting); 
+        }
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Properties.Settings.Default.updateSettings)
+            {
+                Properties.Settings.Default.Upgrade();
+                Properties.Settings.Default.updateSettings = false;
+                Properties.Settings.Default.Save();
+            }
+            MessageBox.Show(Properties.Settings.Default.TestSetting); 
+        }
+    }
+}
